3f775c06a67bab5a9488eaf78729e29d1d1575bf
Akshara Sharma <akshara582582@gmail.com>
2023-07-03 20:32:57 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (85.37%) - LeetSync

--- buddy-strings/buddy-strings.cpp
+++ buddy-strings/buddy-strings.cpp
@@ -1,41 +1,91 @@
+
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
-        if(s.size()!= goal.size()){
-            return false;
-
+        int n = s.length();
+        if(s == goal){
+            set<char> temp(s.begin(), s.end());
+            return temp.size() < goal.size(); // Swapping same characters
         }
 
-        if(s==goal){
-            vector<int> frequency(26,0);
-            for(auto& ch : s){
-               frequency[ch - 'a'] += 1;
-               if(frequency[ch - 'a'] == 2){
-                   return true;
-               }
-            }
-            return false;
-        }
-        int fInd = -1;
-        int sInd = -1;
-        
-        for(int i=0; i<s.size(); ++i){
-            if(s[i] != goal[i]){
-                if(fInd == -1){
-                    fInd = i;
-                } else if(sInd == -1 ){
-                    sInd = i;
-                }else{
-                    return false;
-                }
-            }
+        int i = 0;
+        int j = n - 1;
+
+        while(i < j && s[i] == goal[i]){
+            i++;
         }
 
-        if(sInd == -1){
-            return false;
+        while(j >= 0 && s[j] == goal[j]){
+            j--;
         }
 
-        return s[fInd] == goal[sInd] && s[sInd] == goal[fInd];
+        if(i < j){
+            swap(s[i], s[j]);
+        }
 
+        return s == goal;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool buddyStrings(string s, string goal) {
+//         if(s.size()!= goal.size()){
+//             return false;
+
+//         }
+
+//         if(s==goal){
+//             vector<int> frequency(26,0);
+//             for(auto& ch : s){
+//                frequency[ch - 'a'] += 1;
+//                if(frequency[ch - 'a'] == 2){
+//                    return true;
+//                }
+//             }
+//             return false;
+//         }
+//         int fInd = -1;
+//         int sInd = -1;
+        
+//         for(int i=0; i<s.size(); ++i){
+//             if(s[i] != goal[i]){
+//                 if(fInd == -1){
+//                     fInd = i;
+//                 } else if(sInd == -1 ){
+//                     sInd = i;
+//                 }else{
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         if(sInd == -1){
+//             return false;
+//         }
+
+//         return s[fInd] == goal[sInd] && s[sInd] == goal[fInd];
+
+//     }
+// };
